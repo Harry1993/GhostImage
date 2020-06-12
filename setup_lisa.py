a3233d1e6ac58301266660698d90392f274cf96d
Yanmao Man <dlut.manym@gmail.com>
2020-06-12 09:43:09 -0700

    tensorflow.keras

--- setup_lisa.py
+++ setup_lisa.py
@@ -4,11 +4,10 @@ import numpy as np
 from PIL import Image
 
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.utils import np_utils
-from keras.models import load_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.models import load_model
 
 class LISAModel:
     num_channels = 3
